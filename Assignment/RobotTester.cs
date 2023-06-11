a33330d61183e7b7d47d60286e3f91046c360355
Nishad7814 <131094974+Nishad7814@users.noreply.github.com>
2023-06-11 05:16:28 +0000

    Documentations

--- Assignment/RobotTester.cs
+++ Assignment/RobotTester.cs
@@ -2,27 +2,44 @@
 
 namespace Assignment
 {
+    /// <summary>
+    /// Represents a simple robot tester class.
+    /// </summary>
     public static class RobotTester
     {
+        /// <summary>
+        /// Tests the robot by assigning and running commands.
+        /// </summary>
         public static void TestRobot()
         {
+            // Create a new instance of the Robot class
             Robot newRobot = new();
+
+            // Display available commands to the user
             Console.WriteLine("Give 6 commands to the robot. Possible commands are:");
             Console.WriteLine("on, off, north, south, east, west");
 
-            for (int input = 0; input < 6; input++)
+            int input = 0;
+            while (input < 6)
             {
                 Console.Write($"Assign command #{input + 1}: ");
                 bool commandAdded = AssignCommand(newRobot, Console.ReadLine());
-                if (!commandAdded)
+                if (commandAdded)
                 {
-                    input--;
+                    input++;
                 }
             }
 
+            // Run the robot
             newRobot.Run();
         }
 
+        /// <summary>
+        /// Assigns a command to the robot based on the user input.
+        /// </summary>
+        /// <param name="robot">The Robot object.</param>
+        /// <param name="command">The user input representing the command.</param>
+        /// <returns>True if the command was added successfully, false otherwise.</returns>
         private static bool AssignCommand(Robot robot, string? command)
         {
             switch (command)
